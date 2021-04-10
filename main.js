bd37841e177839d3237cc3bfa0f7d281bc2f04b3
Maria DelSignore <maria.ad27@gmail.com>
2021-04-09 18:55:31 -0600

    Initial function

---
+++ main.js
@@ -0,0 +1,16 @@
+// when a user clicks letsCookBtn,
+// a random dish is populated from the list of possible dishes for that category
+// when the dish is displayed, the cookpot img is hidden
+
+var sides = [];
+var mains = [];
+var desserts = [];
+
+
+var letsCookBtn = document.querySelector('#lets-cook-btn');
+
+letsCookBtn.addEventListener('click', populateDish);
+
+function populateDish() {
+
+}
