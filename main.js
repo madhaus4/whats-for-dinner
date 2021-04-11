984a1dc888086c0eb0efffb0b1def8611d7c21c5
Maria DelSignore <maria.ad27@gmail.com>
2021-04-11 12:27:47 -0600

    Create populateDish functionality

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var sideOption = document.querySelector('#side');
 var mainOption = document.querySelector('#mainDish');
 var dessertOption = document.querySelector('#dessert');
-var cookpotImg = document.querySelector('#cookpotImg');
+var cookpotImg = document.querySelector('img');
 var youShouldMake = document.querySelector('#youShouldMake');
 var menuItem = document.querySelector('#menuItem');
 
@@ -9,45 +9,34 @@ var letsCookBtn = document.querySelector('#lets-cook-btn');
 var clearBtn = document.querySelector('#clearBtn');
 
 //letsCookBtn.addEventListener('click', populateDish);
-letsCookBtn.addEventListener('click', populateDish);
-
+letsCookBtn.addEventListener('click', function() {
+  populateDish();
+  displayDish();
+});
 
 function randomDishItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-// function populateDish() {
-//   event.preventDefault();
-//   var foodItem = '';
-//   if (sideOption.checked) {
-//     foodItem = randomDishItem(sides);
-//   } else if (mainOption.checked) {
-//     foodItem = randomDishItem(mains);
-//   } else if (dessertOption.checked) {
-//     foodItem = randomDishItem(desserts);
-//   }
-//   console.log(foodItem);
-//   return foodItem;
-//   displayDish();
-// }
-
+var foodItem = '';
 function populateDish() {
   event.preventDefault();
   if (sideOption.checked) {
-    displayDish(sides);
+    foodItem = randomDishItem(sides);
   } else if (mainOption.checked) {
-    displayDish(mains);
+    foodItem = randomDishItem(mains);
   } else if (dessertOption.checked) {
-    displayDish(desserts);
+    foodItem = randomDishItem(desserts);
   }
+  console.log(foodItem);
 }
 
-var mealSelection = '';
-function displayDish(meal) {
-  mealSelection = meal[randomDishItem(meal)];
+function displayDish() {
+  //console.log(mealSelection);
+  //foodItem = meal[randomDishItem(meal)];
   cookpotImg.classList.add('visibility-hidden');
   youShouldMake.classList.remove('visibility-hidden');
   menuItem.classList.remove('visibility-hidden');
   clearBtn.classList.remove('visibility-hidden');
-  cookpotImg.innerHTML += `<p>${mealSelection}</p>`
+  menuItem.innerHTML += `<p>${foodItem}</p>`
 }
