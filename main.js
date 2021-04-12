792d51f640dc820b7c8a15fd8490f3f4dba16c00
Maria DelSignore <maria.ad27@gmail.com>
2021-04-11 19:34:58 -0600

    Start over

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 var sideOption = document.querySelector('#side');
 var mainOption = document.querySelector('#mainDish');
 var dessertOption = document.querySelector('#dessert');
+
 var cookpotImg = document.querySelector('img');
+
+var dish = document.querySelector('#displayId');
 var youShouldMake = document.querySelector('#youShouldMake');
 var menuItem = document.querySelector('#menuItem');
+
 var letsCookBtn = document.querySelector('#lets-cook-btn');
 var clearBtn = document.querySelector('#clearBtn');
-var foodItem = '';
+
 
 letsCookBtn.addEventListener('click', function() {
   populateDish();
   displayDish();
 });
-clearBtn.addEventListener('click', clearSelection);
 
 function randomDishItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+var foodItem = '';
 function populateDish() {
   event.preventDefault();
   if (sideOption.checked) {
@@ -27,25 +31,20 @@ function populateDish() {
   } else if (dessertOption.checked) {
     return foodItem = randomDishItem(desserts);
   }
-  alert('You didn\'t choose an option');
+  console.log(foodItem);
 }
 
 function displayDish() {
-  cookpotImg.classList.add('visibility-hidden');
-  youShouldMake.classList.remove('visibility-hidden');
-  menuItem.classList.remove('visibility-hidden');
-  clearBtn.classList.remove('visibility-hidden');
-  menuItem.innerHTML += `<p>${foodItem}!</p>`;
+  menuItem.innerHTML += `${foodItem}!`;
+
+  cookpotImg.classList.add('hidden');
+  dish.classList.remove('hidden');
+  youShouldMake.classList.remove('hidden');
+  menuItem.classList.remove('hidden');
 }
 
-function clearSelection() {
-  var clearMenuItem = event.target.className('clear-btn');
-  for (var i = 0; i < menuItem.length; i++) {
-    menuItem.innerHTML += '';
-  }
-  // cookpotImg.classList.remove('visibility-hidden');
-  // youShouldMake.classList.add('visibility-hidden');
-  // menuItem.classList.add('visibility-hidden');
-  // clearBtn.classList.add('visibility-hidden');
-
-}
+  //
+  // menuItem.innerHTML = '';
+  // for (var i = 0; i < menuItem.length; i++) {
+  //   menuItem.innerHTML += '';
+  // }
